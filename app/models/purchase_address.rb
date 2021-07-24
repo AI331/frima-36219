57fc99b606df9c15ea8fdabc7033d9bde21239d0
AI331 <akira0331ai@gmail.com>
2021-07-24 16:10:17 +0900

    purchaseとaddressのモデル、コントローラ作成。formオブジェクト実装

---
+++ app/models/purchase_address.rb
@@ -0,0 +1,19 @@
+class PurchaseAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :delivery_id, :municipality, :address, :building_name, :phone_number
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, allow_blank: true, message: "is invalid. Include hyphen(-)"}
+    validates :delivery_id
+    validates :municipality
+    validates :address
+    validates :phone_number
+  end
+
+  def save
+    purchase = Purchase.create(item_id: @item_id.id, user_id: user_id)
+    Address.create(postal_code: postal_code, delivery_id: delivery_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
+  end
+end
