4b34e4aa3d747a5c9c524c3963b21db7002c0162
AI331 <akira0331ai@gmail.com>
2021-07-26 20:35:02 +0900

    rubocop実行

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,5 @@
 class PurchasesController < ApplicationController
-  before_action :set_item, only: [:index,:sold_out_item]
+  before_action :set_item, only: [:index, :sold_out_item]
   before_action :sold_out_item, only: [:index]
 
   def index
@@ -10,11 +10,11 @@ class PurchasesController < ApplicationController
     @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
+      Payjp.api_key = ENV['PAYJP_SECRET_KEY']
       Payjp::Charge.create(
-        amount: @item[:price], 
-        card: purchase_params[:token], 
-        currency: 'jpy'                 
+        amount: @item[:price],
+        card: purchase_params[:token],
+        currency: 'jpy'
       )
       @purchase_address.save
       redirect_to root_path
@@ -26,13 +26,13 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_address).permit(:postal_code, :delivery_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item, token: params[:token])
+    params.require(:purchase_address).permit(:postal_code, :delivery_id, :municipality, :address, :building_name, :phone_number).merge(
+      user_id: current_user.id, item_id: @item, token: params[:token]
+    )
   end
 
   def sold_out_item
-    if @item.purchase.present?
-      redirect_to root_path
-    end
+    redirect_to root_path if @item.purchase.present?
   end
 
   def set_item
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -5,16 +5,17 @@ class PurchaseAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, allow_blank: true, message: "is invalid. Include hyphen(-)"}
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, allow_blank: true, message: 'is invalid. Include hyphen(-)' }
     validates :delivery_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :municipality
     validates :address
-    validates :phone_number, format: {with: /\A\d{10,11}\z/}
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
     validates :token
   end
 
   def save
     purchase = Purchase.create(item_id: @item_id.id, user_id: user_id)
-    Address.create(postal_code: postal_code, delivery_id: delivery_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
+    Address.create(postal_code: postal_code, delivery_id: delivery_id, municipality: municipality, address: address,
+                   building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
